f02dd76ec833aba1bc5d6448a99b46a650db5cc2
odiseaalespacio <matii.vargas@duocuc.cl>
2024-06-19 15:18:58 -0400

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -1,2 +1,7 @@
 # este es el archivo pýthon en el cual mis compañeros y yo 
 # trabajaremos para crear una calculadora de manera colaborativa
+
+def sumar(numero1,numero2):
+    return numero1+numero2
+def restar(numero1,numero2):
+    return numero1-numero2
