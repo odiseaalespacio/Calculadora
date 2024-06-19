017858aa657e182ba76869a024152a4a165a92f9
niparrav <ni.parrav@duocuc.cl>
2024-06-19 15:20:03 -0400

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -1,2 +1,8 @@
 # este es el archivo pýthon en el cual mis compañeros y yo 
 # trabajaremos para crear una calculadora de manera colaborativa
+
+def multiplicar(num1, num2):
+    resultadoM = num1 * num2;
+    return resultadoM;
+
+def division()
