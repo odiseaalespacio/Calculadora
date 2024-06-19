5b2478ba8da065dcc9c390237445be6125d82e74
niparrav <ni.parrav@duocuc.cl>
2024-06-19 15:33:29 -0400

    push

--- calculadora.py
+++ calculadora.py
@@ -1,8 +1,2 @@
 # este es el archivo pýthon en el cual mis compañeros y yo 
 # trabajaremos para crear una calculadora de manera colaborativa
-
-def sumar(numero1,numero2):
-    return numero1+numero2
-def restar(numero1,numero2):
-    return numero1-numero2
-
